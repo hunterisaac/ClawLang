b03b80d1ef9b5f975eb2dd6e2f826871248f8263
Hunter Li <hunterisaac2009@gmail.com>
2026-05-04 20:24:48 -0700

    Getting rid of the None results in RAG response
    WAY harder than it looks

--- testing.py
+++ testing.py
@@ -21,8 +21,31 @@ for i in arr:
     for x in range(len(chunks)):
         ids.append(f"{i}_{x}")
         texts.append(chunks[x])
+    f.close()
+
 collection.add(ids = ids, documents=texts) 
-print(collection.query(
+results = collection.query(
     query_texts=["what is the password"], #semantic match
-    n_results=1,
-))
+    n_results=2,
+)
+removal = []
+print(results)
+def remove_nones(x):
+        if isinstance(x, list):
+            cleaned = [remove_nones(i) for i in x if i is not None]
+            return [i for i in cleaned if i != []]
+        return x
+for key, data in results.items(): #my friend helped me with this recursion
+    cleaned = remove_nones(data)
+    if not cleaned:
+        removal.append(key)
+    print(cleaned)
+    results[key] = cleaned
+for i in removal:
+    results.pop(i)
+
+print(results)
+
+
+
+
