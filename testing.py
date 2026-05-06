14587bf26901c043cde45f27b7bc91d175ebbab7
Hunter Li <hunterisaac2009@gmail.com>
2026-05-06 16:49:11 -0700

    Removed the unnecessary algorithm
    RIP lol spent so long on it

--- testing.py
+++ testing.py
@@ -1,63 +1,51 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 import chromadb 
-def create_docs(collection):
-    CHUNK_SIZE = 500
-    CHUNK_OVERLAP = 50
+client = chromadb.PersistentClient()
+collection = client.get_or_create_collection(
+        name="RAG",
+    )
+query = "what is the password"
+dir = 'rag_docs'
+def create_docs(collection, dir):
     splitter = RecursiveCharacterTextSplitter( #taken from the docs website
-        chunk_size=CHUNK_SIZE,
-        chunk_overlap=CHUNK_OVERLAP,
+        chunk_size=500,
+        chunk_overlap=50,
         separators=["\n\n", "\n", ". ", " "]
     )
-    dir = 'rag_docs'
+    
     arr = os.listdir(dir)
+    
     ids = []
     texts = []
     for i in arr:
-        f = open(f"{dir}/{i}")
-        text = f.read()
+        if not i.endswith('.txt'): #only accepts .txt
+            with open(f"{dir}/{i}", "r") as f:
+                text = f.read()
         chunks = splitter.split_text(text)
         for x in range(len(chunks)):
             ids.append(f"{i}_{x}")
             texts.append(chunks[x])
-        f.close()
-
+    if not ids:
+        print('No ids')
+        return
     collection.upsert(ids = ids, documents=texts) 
-def search_docs(query, collection):
-    
+def search_docs(query, collection, k_results):
+    MAX_CHARS = 6000
+    final = ""
     results = collection.query(
         query_texts=[query], #semantic match
-        n_results=3,
+        n_results=k_results,
     )
-    removal = []
-    def remove_nones(x):
-        if isinstance(x, list):
-            cleaned = [remove_nones(i) for i in x if i is not None]
-            return [i for i in cleaned if i != []]
-        elif isinstance(x, dict):
-            cleaned = {
-                k: remove_nones(v)
-                for k, v in x.items()
-                if v is not None
-            }
-            return {k: v for k, v in cleaned.items() if v != {} and v != []}
+    citations = results['ids'][0]
+    text = results['documents'][0]
+    for i in range(len(citations)): #realized i didn't need the crazy loops lol
+        chunk = f"Source: {citations[i]}, Content: {text[i]} \n"
+        if len(final) + len(chunk) > MAX_CHARS:
+            break
+        final = final + chunk
 
-        return x
-    for key, data in results.items(): #my friend helped me with this recursion
-        cleaned = remove_nones(data)
-        if not cleaned:
-            removal.append(key)
-        results[key] = cleaned
-    for i in removal:
-        results.pop(i)
+    return(final.strip())
+print(search_docs(query, collection, k_results=5))
+#create_docs(collection, dir)
 
-    print(results)
-
-query = "what is the password"
-client = chromadb.PersistentClient()
-
-collection = client.get_or_create_collection(
-    name="RAG",
-)
-#search_docs(query, collection)
-create_docs(collection)
--- testing2.py
+++ testing2.py
@@ -1,28 +1,9 @@
 #results = {'ids': [['secret.txt_0', 'alpha.txt_0']], 'embeddings': None, 'documents': [['Implementing a rag code stuff\nSecret code is 19854', 'Nothing useful here!']], 'uris': None, 'included': ['metadatas', 'documents', 'distances'], 'data': None, 'metadatas': [[None, None]], 'distances': [[1.437341570854187, 1.6425095796585083]]}
-results = {'ids': ['hi', None, 'yes', [None, 'hi']], 'dicts':{'htsdfdsf':None, 'dude': {'hi': 'hi', 'bra': None}}}
+results = {'ids': [['secret.txt_0', 'alpha.txt_0', 'blop.txt_0', 'yadda.txt_0', 'roop.txt_0']], 'documents': [['Implementing a rag code stuff\nSecret code is 19854', 'Nothing useful here!', 'Why are you here lol', 'Who are you?!', "How's it going?"]], 'included': ['metadatas', 'documents', 'distances'], 'distances': [[1.437341570854187, 1.6425095796585083, 1.7057721614837646, 1.7472952604293823, 1.89181387424469]]}
 removal = []
 
-def remove_nones(x):
-    if isinstance(x, list):
-        cleaned = [remove_nones(i) for i in x if i is not None]
-        return [i for i in cleaned if i != []]
+documents = results['ids'][0]
+text = results['documents'][0]
 
-    elif isinstance(x, dict):
-        cleaned = {
-            k: remove_nones(v)
-            for k, v in x.items()
-            if v is not None
-        }
-        return {k: v for k, v in cleaned.items() if v != {} and v != []}
-
-    return x
-for key, data in results.items(): #my friend helped me with this recursion
-    cleaned = remove_nones(data)
-    if not cleaned:
-        removal.append(key)
-    print(cleaned)
-    results[key] = cleaned
-for i in removal:
-    results.pop(i)
-
-print(results)
+for i in range(len(documents)): #realized i didn't need the crazy loops lol
+    print(f"Source: documents[i], Content: text[i]")
