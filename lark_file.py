3dc81849192eed105ee5fb856b4376394df4e8d3
Hunter Li <hunterisaac2009@gmail.com>
2026-05-14 21:25:43 -0700

    Error handling
    Awesome

--- lark_file.py
+++ lark_file.py
@@ -1,6 +1,7 @@
 import logging
-from lark import Lark, logger
+from lark import Lark, UnexpectedInput
 from lark.indenter import Indenter
+
 grammar = r"""
 start: statement+
 statement: use_stm | knowledge_stm | agent_stm | main_stm | print_stm
@@ -16,9 +17,9 @@ agent_stm: "agent" IDENTIFIER ":" _NL _INDENT system_p tool_args? _DEDENT
 system_p: "persona:" string _NL?
 tool_args: "tools:" "[" (IDENTIFIER ("," IDENTIFIER)*)? "]" _NL?
 
-main_stm: "flow" IDENTIFIER "(" IDENTIFIER ")" ":" _NL _INDENT workflow _DEDENT
+main_stm: "flow" IDENTIFIER "(" IDENTIFIER ")" ":" _NL _INDENT (workflow | print_stm)+ _DEDENT
 workflow: IDENTIFIER (">>" (FLOW  | IDENTIFIER ) )+ _NL
-print_stm: "print" IDENTIFIER _NL
+print_stm: "print" IDENTIFIER _NL?
 
 %import common.ESCAPED_STRING
 %import common.INT
@@ -32,21 +33,8 @@ _NL: (/\r?\n[\t ]*/ | SH_COMMENT)+
 %ignore " "
 %declare _INDENT _DEDENT
 """
-text = '''use provider Mistral(model="mistral/mistral-tiny")
-knowledge secret_docs:
-    source: "./rag_docs"
-    top_k: 3
-agent Hacker: 
-    persona: "You are a master hacker. You think out of bounds and in unique ways with the tools given to you."
-    tools: [add, multiply]
-flow Main(query):
-    secret_docs >> Hacker(query) >> answer 
-print answer 
-'''  
-
-
-
-
+with open(r"C:\Users\Hunter\Documents\GitHub\Python-Transpiler\lark_files\north_star.ai") as f:
+    text = f.read()
 
 class TreeIndenter(Indenter):
     NL_type = '_NL'
@@ -55,6 +43,11 @@ class TreeIndenter(Indenter):
     INDENT_type = '_INDENT'
     DEDENT_type = '_DEDENT'
     tab_len = 8
-parser = Lark(grammar, parser='lalr', postlex=TreeIndenter())
-tree = parser.parse(text)
-print(tree.pretty())
+try:
+    parser = Lark(grammar, parser='lalr', postlex=TreeIndenter())
+    tree = parser.parse(text)
+    print(tree.pretty())
+except UnexpectedInput as e:
+    print("Error on line:", e.line, e)
+
+
