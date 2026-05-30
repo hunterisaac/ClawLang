a9df03471794df1ca5a9965169eec5f310133662
Hunter Li <hunterisaac2009@gmail.com>
2026-05-29 18:57:21 -0700

    Merging into my Lark file

--- lark_file.py
+++ lark_file.py
@@ -92,3 +92,24 @@ class CompileError(Exception):
         self.value = value
     def __str__(self):
         return f"CompileError at line {self.line} in [{self.node_type}]: {self.message} '{self.value}'"
+
+string = "hi"
+class writer:
+    def __init__(self):
+        self.lines = []
+        self.ind_level = 0
+        pass
+    def writes(self, string):
+        indent = ""
+        for i in range(self.ind_level):
+            indent = indent + "    "
+        text = indent + string
+        self.lines.append(text)
+    def indent(self):
+       self.ind_level += 1
+    def dedent(self):
+        self.ind_level -= 1
+w = writer()
+w.indent()
+w.writes("hi")
+#print(w.lines)
